2df8489fd499ff74a789465a26f7145cf81bf28b
Manuel Šarić <manujelko@gmail.com>
2023-11-15 09:47:35 +0100

    Configure lsp

--- init.lua
+++ init.lua
@@ -25,7 +25,7 @@ vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
 
 -- should neovim transform tabs into spaces
-vim.opt.expandtab = false
+vim.opt.expandtab = true
 
 -- map leader to space
 vim.g.mapleader = ' '
@@ -82,8 +82,22 @@ lazy.opts = {}
 
 lazy.setup({
 	-- list of plugins
+    -- color theme
 	{'bluz71/vim-moonfly-colors'},
+    -- status line
 	{'nvim-lualine/lualine.nvim'},
+	-- plugins for lsp
+	{'neovim/nvim-lspconfig'},
+    {'williamboman/mason.nvim'},
+    {'williamboman/mason-lspconfig.nvim'},
+	-- plugins for autocompletion
+	{'hrsh7th/nvim-cmp'},
+	{'hrsh7th/cmp-buffer'},
+	{'hrsh7th/cmp-path'},
+	{'saadparwaiz1/cmp_luasnip'},
+	{'hrsh7th/cmp-nvim-lsp'},
+	{'L3MON4D3/LuaSnip'},
+	{'rafamadriz/friendly-snippets'},
 })
 
 -- color theme
@@ -105,4 +119,196 @@ vim.g.netrw_winsize = 30
 
 -- reload config
 vim.api.nvim_create_user_command('ReloadConfig', 'source $MYVIMRC', {})
+
+-- lsp
+require('mason').setup()
+require('mason-lspconfig').setup()
+local lspconfig = require('lspconfig')
+local lsp_capabilities = require('cmp_nvim_lsp').default_capabilities()
+
+-- lua lsp
+lspconfig.lua_ls.setup({
+	settings = {
+		Lua = {
+			diagnostics = {
+				globals = {
+					'vim',
+				},
+			},
+		},
+	},
+	capabilities = lsp_capabilities,
+})
+
+-- lsp keybindings
+vim.api.nvim_create_autocmd('LspAttach', {
+	desc = 'LSP actions',
+	callback = function()
+		local bufmap = function(mode, lhs, rhs)
+			local opts = {buffer = true}
+			vim.keymap.set(mode, lhs, rhs, opts)
+        end
+
+		-- displays hover information about the symbol under the cursor
+		bufmap('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>')
+
+		-- jump to definition
+		bufmap('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>')
+
+		-- jump to declaration
+		bufmap('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>')
+
+		-- list all the implementations for the symbol under the cursor
+		bufmap('n', 'gi', '<cmd"lua vim.lsp.buf.implementation()<cr>')
+
+		-- jumps to the definition of the type symbol
+		bufmap('n', 'go', '<cmd>lua vim.lsp.buf.type_definition()<cr>')
+
+		-- lists all the references
+		bufmap('n', 'gr', '<cmd>lua vim.lsp.buf.references()<cr>')
+
+		-- displays a function's signature information
+		bufmap('n', 'gs', 'Mcmd>lua vim.lsp.buf.signature_help()<cr>')
+
+		-- renames all references to the symbol under the cursor
+		bufmap('n', '<F2>', '<cmd>lua vim.lsp.buf.rename()<cr>')
+
+		-- selects a code action available at the current cursor position
+		bufmap('n', '<F4>', '<cmd>lua vim.lsp.buf.code_action()<cr>')
+
+		-- show diagnosticts in a floating window
+		bufmap('n', 'gl', '<cmd>lua vim.diagnostic.open_float()<cr>')
+
+		-- move to the previous diagnostic
+		bufmap('n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<cr>')
+
+		-- move to the next diagnostic
+		bufmap('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<cr>')
+	end
+})
+
+-- snippets
+require('luasnip.loaders.from_vscode').lazy_load()
+
+-- autocompletion
+vim.opt.completeopt = {'menu', 'menuone', 'noselect'}
+local cmp = require('cmp')
+local luasnip = require('luasnip')
+local select_opts = {behavior = cmp.SelectBehavior.Select}
+cmp.setup({
+    snippet = {
+        expand = function(args)
+            luasnip.lsp_expand(args.body)
+        end
+    },
+    -- data sources to populate the completion list
+    sources = {
+        -- autocomplete file paths
+        {name = 'path'},
+        -- suggestions based on the language server
+        {name = 'nvim_lsp', keyword_length = 1},
+        -- suggest words found in the current buffer
+        {name = 'buffer', keyword_length = 3},
+        -- show available snippets
+        {name = 'luasnip', keyword_length = 2},
+    },
+    -- appearance and settings for the documentation window
+    window = {
+        documentation = cmp.config.window.bordered()
+    },
+    formatting = {
+        -- order of the elements in an item
+        fields = {'menu', 'abbr', 'kind'},
+        -- customize the appearance of the completion menu
+        format = function(entry, item)
+            local menu_icon = {
+                nvim_lsp = 'λ',
+                luasnip = '⋗',
+                buffer = 'Ω',
+                path = '‣',
+            }
+
+            item.menu = menu_icon[entry.source.name]
+            return item
+        end,
+    },
+    -- completion keybindings
+    mapping = {
+        -- confirm selection
+        ['<CR>'] = cmp.mapping.confirm({select = false}),
+        ['<C-y>'] = cmp.mapping.confirm({select = true}),
+        -- cancel completion
+        ['<C-e>'] = cmp.mapping.abort(),
+        -- move between completion items
+        ['<Up>'] = cmp.mapping.select_prev_item(select_opts),
+        ['<Down>'] = cmp.mapping.select_next_item(select_opts),
+        ['<C-p>'] = cmp.mapping.select_prev_item(select_opts),
+        ['<C-n>'] = cmp.mapping.select_next_item(select_opts),
+        -- scroll text in the documentation window
+        ['<C-u>'] = cmp.mapping.scroll_docs(-4),
+        ['<C-d>'] = cmp.mapping.scroll_docs(4),
+        -- jump to the next placeholder in the snippet
+        ['<C-f>'] = cmp.mapping(function(fallback)
+            if luasnip.jumpable(1) then
+                luasnip.jump(1)
+            else
+                fallback()
+            end
+        end, {'i', 's'}),
+        -- jump to the previous placeholder in the snippet
+        ['<C-b>'] = cmp.mapping(function(fallback)
+            if luasnip.jumpable(-1) then
+                luasnip.jump(-1)
+            else
+                fallback()
+            end
+        end, {'i', 's'}),
+        -- autocomplete with tab
+        ['<Tab>'] = cmp.mapping(function(fallback)
+            local col = vim.fn.col('.') - 1
+
+            if cmp.visible() then
+                cmp.select_next_item(select_opts)
+            elseif col == 0 or vim.fn.getline('.'):sub(col, col):match('%s') then
+                fallback()
+            else
+                cmp.complete()
+            end
+        end, {'i', 's'}),
+        ['<S-Tab>'] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+                cmp.select_prev_item(select_opts)
+            else
+                fallback()
+            end
+        end, {'i', 's'}),
+    },
+})
+
+-- change diagnostics icons
+local sign = function(opts)
+    vim.fn.sign_define(opts.name, {
+        texthl = opts.name,
+        text = opts.text,
+        numhl = ''
+    })
+end
+
+sign({name = 'DiagnosticSignError', text = '✘'})
+sign({name = 'DiagnosticSignWarn', text = '▲'})
+sign({name = 'DiagnosticSignHint', text = '⚑'})
+sign({name = 'DiagnosticSignInfo', text = '»'})
+
+vim.diagnostic.config({
+    virtual_text = false,
+    severity_sort = true,
+    float = {
+        border = 'rounded',
+        source = 'awlays',
+    },
+})
+
+-- help windows with borders
+vim.lsp.handlers['textDocument/hover'] = vim.lsp.with(vim.lsp.handlers.hover, {border = 'rounded'})
+vim.lsp.handlers['textDocument/signatureHelp'] = vim.lsp.with(vim.lsp.handlers.signature_help, {border = 'rounded'})
 
