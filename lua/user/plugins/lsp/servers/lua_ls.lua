38d70d29543d264ebcc08a3d880538dba7aadcd8
Manuel Šarić <manujelko@gmail.com>
2024-01-27 22:35:49 +0100

    Some cleanup

--- lua/user/plugins/lsp/servers/lua_ls.lua
+++ lua/user/plugins/lsp/servers/lua_ls.lua
@@ -2,16 +2,24 @@ local common = require('user.plugins.lsp.common')
 local lspconfig = require('lspconfig')
 
 lspconfig.lua_ls.setup({
-    on_attach = common.on_attach,
+    on_attach = function(client, bufnr)
+        common.on_attach(client, bufnr)
+        -- add formatting on save
+        vim.api.nvim_create_autocmd('BufWritePre', {
+            buffer = bufnr,
+            callback = function()
+                vim.lsp.buf.format({ timeout_ms = 2000 })
+            end,
+        })
+    end,
     capabilities = common.capabilities,
     settings = {
         Lua = {
             diagnostics = {
                 globals = {
-                    'vim',
+                    'vim'
                 },
             },
         },
     },
 })
-
--- lua/user/utils/pyright.lua
+++ lua/user/utils/pyright.lua
@@ -1,32 +1,32 @@
 local function pyrightconfig(args)
-    -- Get the Python version from the argument
+    -- Get the provided environment name
     local venv_name = args.args
 
     -- Get the basename of the current working directory if argument not provided
-    if not venv_name or venv_name == "" then
-        local handle = io.popen("basename $(pwd)")
+    if not venv_name or venv_name == '' then
+        local handle = io.popen('basename $(pwd)')
         if handle then
-            venv_name = handle:read("*l")
+            venv_name = handle:read('*l')
             handle:close()
         else
-            print("Error: Unable to execute shell command.")
+            print('Error: Unable to execute shell command.')
             return
         end
     end
 
     -- Define the JSON content
-    local home_directory = os.getenv("HOME")
-    local venv_path = home_directory .. "/.pyenv/versions"
+    local home_directory = os.getenv('HOME')
+    local venv_path = home_directory .. '/.pyenv/versions'
     local content = string.format('{\n    "venvPath": "%s",\n    "venv": "%s"\n}', venv_path, venv_name)
 
     -- Write the content to pyrightconfig.json
-    local file = io.open("pyrightconfig.json", "w")
+    local file = io.open('pyrightconfig.json', 'w')
     if file then
         file:write(content)
         file:close()
-        print("pyrightconfig.json created with venv: " .. venv_name)
+        print('pyrightconfig.json created with venv: ' .. venv_name)
     else
-        print("Error: Unable to open pyrightconfig.json for writing.")
+        print('Error: Unable to open pyrightconfig.json for writing.')
     end
 end
 
@@ -36,5 +36,5 @@ vim.api.nvim_create_user_command(
     function(args)
         pyrightconfig({args = args.args})
     end,
-    { nargs = "?" }
+    { nargs = '?' }
 )
