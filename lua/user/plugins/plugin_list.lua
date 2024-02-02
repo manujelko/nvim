4aadca4f7133440949cd1a8c06a3f58d7bed8fbf
Manuel Šarić <manujelko@gmail.com>
2024-02-02 12:57:50 +0100

    Remove python indentation plugin

--- lua/user/plugins/plugin_list.lua
+++ lua/user/plugins/plugin_list.lua
@@ -3,35 +3,33 @@ local lazy = require('user.plugins.manager')
 lazy.setup({
     -- list of plugins
     -- plenary
-    {'nvim-lua/plenary.nvim'},
+    { 'nvim-lua/plenary.nvim' },
     -- color theme
-    {'bluz71/vim-moonfly-colors'},
+    { 'bluz71/vim-moonfly-colors' },
     -- status line
-    {'nvim-lualine/lualine.nvim'},
+    { 'nvim-lualine/lualine.nvim' },
     -- plugins for lsp
-    {'neovim/nvim-lspconfig'},
-    {'williamboman/mason.nvim'},
-    {'williamboman/mason-lspconfig.nvim'},
+    { 'neovim/nvim-lspconfig' },
+    { 'williamboman/mason.nvim' },
+    { 'williamboman/mason-lspconfig.nvim' },
     -- plugins for autocompletion
-    {'hrsh7th/nvim-cmp'},
-    {'hrsh7th/cmp-buffer'},
-    {'hrsh7th/cmp-path'},
-    {'saadparwaiz1/cmp_luasnip'},
-    {'hrsh7th/cmp-nvim-lsp'},
-    {'L3MON4D3/LuaSnip'},
-    {'rafamadriz/friendly-snippets'},
+    { 'hrsh7th/nvim-cmp' },
+    { 'hrsh7th/cmp-buffer' },
+    { 'hrsh7th/cmp-path' },
+    { 'saadparwaiz1/cmp_luasnip' },
+    { 'hrsh7th/cmp-nvim-lsp' },
+    { 'L3MON4D3/LuaSnip' },
+    { 'rafamadriz/friendly-snippets' },
     -- file explorer
-    {'nvim-tree/nvim-tree.lua'},
+    { 'nvim-tree/nvim-tree.lua' },
     -- fuzzy finder
-    {'nvim-telescope/telescope.nvim'},
-    {'nvim-telescope/telescope-fzf-native.nvim', build = 'make'},
+    { 'nvim-telescope/telescope.nvim' },
+    { 'nvim-telescope/telescope-fzf-native.nvim',   build = 'make' },
     -- comments
-    {'numToStr/Comment.nvim', lazy = false},
+    { 'numToStr/Comment.nvim',                      lazy = false },
     -- treesitter
-    {'nvim-treesitter/nvim-treesitter'},
-    {'nvim-treesitter/nvim-treesitter-textobjects'},
+    { 'nvim-treesitter/nvim-treesitter' },
+    { 'nvim-treesitter/nvim-treesitter-textobjects' },
     -- surround
-    {'tpope/vim-surround'},
-    -- python indent
-    {'Vimjas/vim-python-pep8-indent'},
+    { 'tpope/vim-surround' },
 })
