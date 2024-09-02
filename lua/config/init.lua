0ab55705b56b862fa2f821a1168859834c74c780
Manuel Šarić <manujelko@gmail.com>
2024-09-02 14:09:08 +0200

    Use the moonfly color theme

--- lua/config/init.lua
+++ lua/config/init.lua
@@ -1,3 +1,4 @@
 require("config.keymap")
 require("config.lazy")
+require("config.theme")
 require("config.nvim-tree")
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,3 +1,4 @@
 return {
-  { "nvim-tree/nvim-tree.lua", version = "*", lazy = false }
+  { "nvim-tree/nvim-tree.lua", version = "*", lazy = false },
+  { "bluz71/vim-moonfly-colors", name = "moonfly", lazy = false, priority = 1000 },
 }
