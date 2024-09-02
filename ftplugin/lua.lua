5ad31eebc6e88fb16f6ef4dab392c27832072c74
Manuel Šarić <manujelko@gmail.com>
2024-09-02 13:54:02 +0200

    Add indentation rules for Lua and Python

---
+++ ftplugin/lua.lua
@@ -0,0 +1,8 @@
+-- amount of spaces a tab character occupies
+vim.opt_local.tabstop = 2
+
+-- amount of characters neovim will use to indent a line
+vim.opt_local.shiftwidth = 2
+
+-- should neovim transform tabs into spaces
+vim.opt_local.expandtab = true
