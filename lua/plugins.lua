a16f785d1c651d1a2680193b4f25a53d876df248
Manuel Šarić <manujelko@gmail.com>
2024-09-02 14:19:21 +0200

    Add vim-surround

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -3,4 +3,5 @@ return {
   { "bluz71/vim-moonfly-colors", name = "moonfly", lazy = false, priority = 1000 },
   { "nvim-telescope/telescope.nvim", tag = "0.1.8", dependencies = { "nvim-lua/plenary.nvim" } },
   { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+  { "tpope/vim-surround" },
 }
