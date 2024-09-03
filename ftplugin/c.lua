d933828ab1dca87e29188c569214a9d78fd02573
Manuel Šarić <manujelko@gmail.com>
2024-09-03 15:45:37 +0200

    Change indentation in C

--- ftplugin/c.lua
+++ ftplugin/c.lua
@@ -1,8 +1,8 @@
 -- amount of spaces a tab character occupies
-vim.opt_local.tabstop = 4
+vim.opt_local.tabstop = 2
 
 -- amount of characters neovim will use to indent a line
-vim.opt_local.shiftwidth = 4
+vim.opt_local.shiftwidth = 2
 
 -- should neovim transform tabs into spaces
 vim.opt_local.expandtab = false
